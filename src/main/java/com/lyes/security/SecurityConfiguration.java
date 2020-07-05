c2557d700e695acf9fb4cb8c92e80152bc0a75e6
lyes makhloufi <lyes.makhloufi@outlook.fr>
2020-07-05 17:27:44 +0200

    set inMemoryAuth authentication

--- src/main/java/com/lyes/security/SecurityConfiguration.java
+++ src/main/java/com/lyes/security/SecurityConfiguration.java
@@ -1,13 +1,31 @@
 package com.lyes.security;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.password.NoOpPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
+@EnableWebSecurity
 public class SecurityConfiguration extends WebSecurityConfigurerAdapter{
 	
 	@Override
 	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
 		
+		// set the configuration on the auth object 
+		
+		auth.inMemoryAuthentication()
+				.withUser("lyes")
+				.password("lyes")
+				.roles("USER");
+		
+		
+	}
+	
+	@Bean
+	public PasswordEncoder getPasswordEncoder() {
+		return NoOpPasswordEncoder.getInstance();
 	}
 
 }
